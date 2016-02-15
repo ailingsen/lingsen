df1f23ded48e096e473c61008d09361194bf2ce5
彭彦彰 <Aaron@pengyanangdeMBP.lan>
2016-02-15 17:02:48 +0800

    guanwang

---
+++ officialWebsite/config/main.php
@@ -0,0 +1,34 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php')
+);
+
+return [
+    'id' => 'app-officialWebsite',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'officialWebsite\controllers',
+    'defaultRoute' => 'page/index',
+    'bootstrap' => ['log'],
+    'modules' => [],
+    'components' => [
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+    ],
+    'params' => $params,
+];
---
+++ officialWebsite/controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+namespace officialWebsite\controllers;
+
+/**
+ * Created by PhpStorm.
+ * User: Aaron
+ * Date: 16/2/15
+ * Time: 下午4:05
+ */
+use yii\web\Controller;
+
+class PageController extends Controller
+{
+    public function actionIndex()
+    {
+        echo '111';
+    }
+}
